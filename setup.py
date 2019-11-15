83ba6bcaaa1b8e9d546b9b71f1420df173a16616
Lukáš Moláček <lukas@molacek.net>
2019-11-15 09:21:47 +0100

    Add missing dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     author="Lukáš Moláček",
     author_email="lukas@molacek.net",
     packages=setuptools.find_packages(),
-    install_requires=["xdg", "requests"],
+    install_requires=["xdg", "requests", "svgwrite"],
     entry_points={
         "console_scripts": [
             "get_btc_price = btcmonitor:main"
